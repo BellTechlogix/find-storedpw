aeebf492651e5d919f104b4d241b7c8552dd5bb3
Kristopher Roy <kristopher.roy@gmail.com>
2021-03-25 12:25:00 -0700

    Initial Push

---
+++ find-storedpw.ps1
@@ -0,0 +1,59 @@
+﻿###Created:Mar 25 2021###
+###Modified:Mar 25 2021###
+###Author:Kristopher Roy###
+###Company:Belltechlogix###
+
+#Variables for output and scan location
+$report = "c:\reports\clearinfo.log"
+$Drives = Get-PSDrive -PSProvider 'FileSystem'
+
+#loops through each drive on the filesystem
+foreach($Drive in $drives) {
+$Drive
+#limits the file types to scan
+$files = Get-ChildItem $Drive.Root -Recurse -File -include "*.ps1","*.bat","*.txt","*.doc","*.rtf"|where{$_.fullname -notlike "*.ps1x"}
+$i = 0
+FOREACH($file in $files){
+    $count = $files.count
+        $i++
+        Write-Progress -Activity ("files read...") -Status "read $i of $($count) files" -PercentComplete ($i/$Count*100) 
+        
+		#Checks each file for the string inside of the content
+		If($file|Get-Content| Select-String -Pattern "AsPlainText")
+        {
+            "--------------------------File:--------------------------"| Add-Content $report
+            ($file|select fullname).fullname|Add-Content $report
+            $file|Get-Content| Select-String -Pattern "AsPlainText"| Add-Content $report
+        }
+        If($file|Get-Content| Select-String -Pattern "password")
+        {
+            "--------------------------File:--------------------------"| Add-Content $report
+            ($file|select fullname).fullname|Add-Content $report
+            $file|Get-Content| Select-String -Pattern "password"| Add-Content $report
+        }
+         If($file|Get-Content| Select-String -Pattern "pw=")
+        {
+            "--------------------------File:--------------------------"| Add-Content $report
+            ($file|select fullname).fullname|Add-Content $report
+            $file|Get-Content| Select-String -Pattern "pw="| Add-Content $report
+        }
+         If($file|Get-Content| Select-String -Pattern "pwd=")
+        {
+            "--------------------------File:--------------------------"| Add-Content $report
+            ($file|select fullname).fullname|Add-Content $report
+            $file|Get-Content| Select-String -Pattern "pwd="| Add-Content $report
+        }
+         If($file|Get-Content| Select-String -Pattern "pwd=")
+        {
+            "--------------------------File:--------------------------"| Add-Content $report
+            ($file|select fullname).fullname|Add-Content $report
+            $file|Get-Content| Select-String -Pattern "set/p"| Add-Content $report
+        }
+         If($file|Get-Content| Select-String -Pattern "pwd=")
+        {
+            "--------------------------File:--------------------------"| Add-Content $report
+            ($file|select fullname).fullname|Add-Content $report
+            $file|Get-Content| Select-String -Pattern "pass="| Add-Content $report
+        }
+    }
+}
